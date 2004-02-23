74900a5dd9a3f7413a614c3af978dcb79034e89e
Mike West <mpwest929@gmail.com>
2004-02-22 19:26:15 -0500

    First attempt with creating backbone.js enabled UI

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,40 @@
-$(document).ready(function() {
-	$( ".slider" ).slider();
-});
+/*$(document).ready(function() {
+	$( ".slider" ).slider({
+    value: 50,
+    min: 10,
+    max: 100,
+    step: 10,
+    slide: function(e, ui) {
+      $("#slider-result").html(ui.value);
+    }
+  });
+});*/
+
+
+(function($) {
+  window.Filter = Backbone.Model.extend({
+  });
+
+  window.Filters = Backbone.Collection.extend({
+    model: Filter
+  });
+
+  window.FilterView = Backbone.View.extend({
+    initialize: function() {
+
+    },
+    render: function() {
+      var template = _.template( $("#slider_template").html(), {});
+      this.el.html(template);
+    }
+  });
+
+  $(document).ready(function() {
+    var filter_view = new FilterView({el: $('#sliders')});
+    
+    $("#new_subreddit").click(function() {
+      // User wants to add a new subreddit
+    });
+  });
+})(jQuery);
+
